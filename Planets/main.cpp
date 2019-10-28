c3fe805f63d7b0b2d023b1f758913de867e7e04b
arthurlapertosa <arthurlisboa@cpejr.com.br>
2019-10-28 15:27:25 -0300

    testes

--- Planets/main.cpp
+++ Planets/main.cpp
@@ -1,12 +1,18 @@
 #include "planets.h"
 #include "lista.h"
+#include<ctime>
 int main()
 {
+	time_t inicio, final, total;
+	time(&inicio);
 	planets Teste;
 	Teste.fillPlanets();
 	//Teste.printPlanets();
 	Teste.sort();
 	//Teste.printPlanets();
 	Teste.buildVisitationList();
-	Teste.printList();
+	//Teste.printList();
+	time(&final);
+	total = final - inicio;
+	std::cout << total << " segundos de execucao" << endl;
 }
